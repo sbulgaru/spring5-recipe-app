9749e8f60c81010c7d52db4eca58958fff51b805
sbulgaru <sbulgaru@deloittece.com>
2022-01-14 16:05:23 +0200

    Adding enum

--- spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
@@ -17,14 +17,16 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
+
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -116,5 +118,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
