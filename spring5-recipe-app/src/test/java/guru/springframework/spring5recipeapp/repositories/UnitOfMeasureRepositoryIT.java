74448370294dfd610c5a3853be7ade1eae32002d
sbulgaru <sbulgaru@deloittece.com>
2022-01-19 12:29:04 +0200

    Added Maven failsafe plugin

--- spring5-recipe-app/src/test/java/guru/springframework/spring5recipeapp/repositories/UnitOfMeasureRepositoryIT.java
+++ spring5-recipe-app/src/test/java/guru/springframework/spring5recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,14 +1,20 @@
 package guru.springframework.spring5recipeapp.repositories;
 
+import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
-class UnitOfMeasureRepositoryIT {
+public class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
@@ -17,11 +23,11 @@ class UnitOfMeasureRepositoryIT {
     void setUp() {
     }
 
-//    @Test
-//    void findByDescription() {
-//
-//        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-//
-//        assertEquals("Teaspoon", uomOptional.get().getDescription());
-//    }
+    @Test
+    public void findByDescription() {
+
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
 }
