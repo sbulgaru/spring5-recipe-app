454cbac7dea5573e95275aa60d23e77ef5d37256
sbulgaru <sbulgaru@deloittece.com>
2022-01-21 15:12:36 +0200

    Show ingredients

--- spring5-recipe-app/src/main/java/guru/springframework/commands/IngredientCommand.java
+++ spring5-recipe-app/src/main/java/guru/springframework/commands/IngredientCommand.java
@@ -6,11 +6,13 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class IngredientCommand {
     private Long id;
+    private Long recipeId;
     private String description;
     private BigDecimal amount;
     private UnitOfMeasureCommand uom;
